1ccb356b2ec7b1a36dc974397924bd6d3153b5d9
Davi <davibomfimsantiago@gmail.com>
2020-07-18 23:08:50 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+const { app, BrowserWindow } = require('electron')
+
+function createWindow () {
+  // Cria uma janela de navegação.
+  let win = new BrowserWindow({width: 240,height: 497, show:false, frame: false,transparent: true,})
+
+
+  //carregar o index.html do aplicativo.
+  win.loadFile('index.html')
+  
+  //Bloquear o controle de expandir a tela do usuario
+  win.setResizable(false) 
+
+  //Concerto bug de carregamento de tela
+  win.once("ready-to-show",()=>{
+      win.show();
+    })
+}
+
+app.whenReady().then(createWindow)
